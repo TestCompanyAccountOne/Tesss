1202d2c6282564c8416b869a3adfa6f4bd5a34a3
suzurikawa <suzurikawa_chihiro@tre-inc.com>
2020-06-29 17:08:22 +0900

    コード追加

--- Chapter_02/main.go
+++ Chapter_02/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
 
 func main() {
 	mux := http.NewServeMux()
@@ -8,10 +11,45 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: mux,
 	}
 	server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	threads, err := data.Treads(); if err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/public.navbar.html",
+			"templates/index.html"
+		}
+		private_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/private.navbar.html",
+			"templates/index.html"
+		}
+		var templates *template.template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+		} else {
+			templates = template.Must(template.ParseFiles(public_tmplfiles...))
+		}
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
 }
